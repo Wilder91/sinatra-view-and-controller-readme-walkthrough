5e4204d5347c530203b086b1c4320c1e9aa4bc09
Mike Wilder <mikewilder01@gmail.com>
2021-01-21 17:03:37 -0500

    commit tree 77b97b0c4bf3ed13e6e66de716dc37d39396a49b

--- app.rb
+++ app.rb
@@ -2,15 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reverse' do
+    originalstring = params["string"] 
+    @reversedstring = original_string.reverse
     erb :reverse
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+  
     erb :reversed
   end
 
   get '/friends' do
-    # Write your code here!
-
-  end
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+  
+    erb :friends
+  end 
 end
